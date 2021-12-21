f7b645b94ceff84c2ef4679a7f9f0a55f45d4965
deepak <ddalavi@itopia.com>
2021-12-21 17:46:07 +0530

    Updated helm version: 1.1.1

---
+++ setup/scripts/release.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+
+HELM_REPO="helm.regelcloud.com"
+charts=$(basename charts/*)
+
+RELEASE_VERSION_MAJOR=$(cut -d'.' -f1 < release)
+RELEASE_VERSION_MINOR=$(cut -d'.' -f2 < release)
+RELEASE_VERSION_PATCH=$(cut -d'.' -f3 < release)
+
+echo "[INFO] Current release version = ${RELEASE_VERSION_MAJOR}.${RELEASE_VERSION_MINOR}.${RELEASE_VERSION_PATCH}"
+echo "[INFO] New release version = ${RELEASE_VERSION_MAJOR}.${RELEASE_VERSION_MINOR}.$(expr ${RELEASE_VERSION_PATCH} + 1)"
+RELEASE_VERSION_LATEST="${RELEASE_VERSION_MAJOR}.${RELEASE_VERSION_MINOR}.$(expr ${RELEASE_VERSION_PATCH} + 1)"
+echo "${RELEASE_VERSION_MAJOR}.${RELEASE_VERSION_MINOR}.$(expr ${RELEASE_VERSION_PATCH} + 1)" > release
+
+for chart in ${charts}
+do 
+    echo "${chart}"
+    sed  -i '' "s/version:.*/version: ${RELEASE_VERSION_LATEST}/g" charts/"${chart}"/Chart.yaml
+done
+
+git pull
+git add .
+git commit -m "Updated helm version: ${RELEASE_VERSION_LATEST}"
+git push
+echo "[INFO] Success build & push to ${HELM_REPO}/${chart} ${RELEASE_VERSION_LATEST}"
