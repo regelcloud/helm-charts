7e43d34f0cc41714abe91b1ed79ea34c0e223ab4
deepak <ddalavi@itopia.com>
2021-12-24 11:03:44 +0530

    Updated helm charts version: 1.1.6

--- setup/scripts/release.sh
+++ setup/scripts/release.sh
@@ -4,39 +4,23 @@ set -e
 HELM_REPO="helm.regelcloud.com"
 charts=$(basename charts/*)
 
-while getopts c: flag
-do
-    case "${flag}" in
-        c) chart=${OPTARG};;
-    esac
-done
-
-if [ -z $chart ] ; then
-    echo "Enter valid chart name: ${charts}"
-    exit 1
-fi
-
-
-
-RELEASE_VERSION_MAJOR=$(cat charts/${chart}/Chart.yaml | grep -w 'version:' | cut -d ':' -f2 | cut -d '.' -f1)
-RELEASE_VERSION_MINOR=$(cat charts/${chart}/Chart.yaml | grep -w 'version:' | cut -d ':' -f2 | cut -d '.' -f2)
-RELEASE_VERSION_PATCH=$(cat charts/${chart}/Chart.yaml | grep -w 'version:' | cut -d ':' -f2 | cut -d '.' -f3)
+RELEASE_VERSION_MAJOR=$(cut -d'.' -f1 < release)
+RELEASE_VERSION_MINOR=$(cut -d'.' -f2 < release)
+RELEASE_VERSION_PATCH=$(cut -d'.' -f3 < release)
 
 echo "[INFO] Current release version = ${RELEASE_VERSION_MAJOR}.${RELEASE_VERSION_MINOR}.${RELEASE_VERSION_PATCH}"
 echo "[INFO] New release version = ${RELEASE_VERSION_MAJOR}.${RELEASE_VERSION_MINOR}.$(expr ${RELEASE_VERSION_PATCH} + 1)"
 RELEASE_VERSION_LATEST="${RELEASE_VERSION_MAJOR}.${RELEASE_VERSION_MINOR}.$(expr ${RELEASE_VERSION_PATCH} + 1)"
-echo "${RELEASE_VERSION_MAJOR}.${RELEASE_VERSION_MINOR}.$(expr ${RELEASE_VERSION_PATCH} + 1)" 
+echo "${RELEASE_VERSION_MAJOR}.${RELEASE_VERSION_MINOR}.$(expr ${RELEASE_VERSION_PATCH} + 1)" > release
 
-for c in ${charts}
+for chart in ${charts}
 do 
-    if [[ ${chart} -eq $c ]]; then
-        echo "${chart}"
-        sed  -i '' "s/version:.*/version: ${RELEASE_VERSION_LATEST}/g" charts/"${chart}"/Chart.yaml
-        git pull
-        git add .
-        git commit -m "Updated helm chart ${chart} to version: ${RELEASE_VERSION_LATEST}"
-        git push
-        echo "[INFO] Success build & push to ${HELM_REPO}/${chart} ${RELEASE_VERSION_LATEST}"
-    fi
+    echo "${chart}"
+    sed  -i '' "s/version:.*/version: ${RELEASE_VERSION_LATEST}/g" charts/"${chart}"/Chart.yaml
 done
 
+git pull
+git add .
+git commit -m "Updated helm charts version: ${RELEASE_VERSION_LATEST}"
+git push
+echo "[INFO] Success build & push to ${HELM_REPO}: ${RELEASE_VERSION_LATEST}"
